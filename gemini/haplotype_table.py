a2998417e024a976ec41ffbe7bdf8f6c851c6b02
proton <chadapohn.chaosrikul@gmail.com>
2019-10-31 09:43:49 +0700

    create haplotype objects

--- gemini/haplotype_table.py
+++ gemini/haplotype_table.py
@@ -1,11 +1,37 @@
+from . import iupac
+
 class haplotype:
 
     def __init__(self, col):
         col = [None if c == 'None' else c for c in col]
         self.cols = col[:]
-        self.gene_symbol = col[0]
+        self.gene = col[0]
         self.name = col[1]
         self.num_variants = col[2]
+        self.starts = col[3]
+        self.ends = col[4]
+        self.chrom_hgvs_names = col[5]
+        self.rsids = col[6]
+        self.alleles = col[7]
+        self.types = col[8]
 
     def __str__(self):
-        return ",".join([self.gene_symbol, self.name, self.num_variants])
+        return ",".join([self.gene, self.name, self.num_variants,
+                        self.starts, self.ends, self.chrom_hgvs_names,
+                        self.rsids, self.alleles, self.types])
+
+class  haplotype_alleles:
+    def __init__(self, col):
+        col = [None if c == 'None' else c for c in col]
+        self.cols = col[:]
+        self.start = col[0]
+        self.end = col[1]
+        self.chrom_hgvs_name = col[2]
+        self.rsid = col[3]
+        self.allele = col[4]
+        self._iupac_pattern = "[" + ''.join(iupac.lookup(self.allele)) + "]"
+        self.type = col[6]
+
+    def __str__(self):
+        return ",".join([self.start, self.end, str(self.chrom_hgvs_name),
+                        self.rsid, self.allele, self._iupac_pattern, self.type])
