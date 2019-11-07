906b361654b7868dccc9b2a9028fdcdb1737f1a6
proton <chadapohn.chaosrikul@gmail.com>
2019-11-07 09:01:41 +0700

    add chrom into the query of the phased_data_haplotype_alleles

--- test/test-diplotype-support.sh
+++ test/test-diplotype-support.sh
@@ -9,12 +9,13 @@ check()
 ###########################################################################
 #1. Test loading no called genotypes 
 ###########################################################################
-gemini load -v non-variants-included-snippet.vcf \
+gemini load -v non-variants-included-snippet.vcf.gz \
 --skip-gene-tables --skip-gerp-bp --skip-cadd non-variants-included-snippet.db
 
 echo "    load.t1...\c"
 echo "chr1	96078437	96078438	A	
-chr1	96078438	96078439	T	C" > exp
+chr1	96078438	96078439	T	C
+chr10	94775505	94775506	C	" > exp
 
 gemini query -q "select chrom, start, end, ref, alt from variants" non-variants-included-snippet.db > obs
 check obs exp
@@ -25,9 +26,9 @@ rm obs exp
 ###########################################################################
 echo "    load.t2...\c"
 echo "gene	name	num_variants
-CAC1NAS	Reference	2
-CAC1NAS	c.520C>T	2
-CAC1NAS	c.3257	2
+CACNA1S	Reference	2
+CACNA1S	c.520C>T	2
+CACNA1S	c.3257	2
 CYP2C19	*2	1" > exp
 
 gemini query --header -q "select gene, name, num_variants from haplotypes" non-variants-included-snippet.db > obs
@@ -39,11 +40,11 @@ rm obs exp
 # the haplotypes table 
 ###########################################################################
 echo "    load.t3...\c"
-echo "gene	start	end	chrom_hgvs_name	rsid	type
-CAC1NAS	201060814	201060815	g.201060815C>T	rs1800559	snp
-CAC1NAS	201091992	201091993	g.201091993G>A	rs772226819	snp" > exp
+echo "gene	chrom	start	end	chrom_hgvs_name	rsid	type
+CACNA1S	chr1	201060814	201060815	g.201060815C>T	rs1800559	snp
+CACNA1S	chr1	201091992	201091993	g.201091993G>A	rs772226819	snp" > exp
 
-gemini query --header -q "select h.gene, a.start, a.end, \
+gemini query --header -q "select h.gene, a.chrom, a.start, a.end, \
 a.chrom_hgvs_name, a.rsid, a.type from haplotypes h inner join phased_data_haplotype_alleles a \
 on h.uid = a.hap_id where h.uid = 1" non-variants-included-snippet.db > obs
 check obs exp
